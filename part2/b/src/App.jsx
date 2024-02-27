005b983628ccc1a4c5b4368379b4d183cfa16dd3
bbqw-ng <brwng902@gmail.com>
2024-02-27 13:13:49 -0500

    created a new directory for part 2 b and initialized react template

---
+++ part2/b/src/App.jsx
@@ -0,0 +1,38 @@
+import Note from './components/Note.jsx'
+//Left Off: Control Component
+import {useState} from 'react'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  const addNote = (event) => {
+    //default actions reload the page, we don't want that so we do event.preventDefault
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
