4cb1d6e0d926fcd67208a13d94e36f7d761c4952
bbqw-ng <brwng902@gmail.com>
2024-03-07 15:10:08 -0500

    added the number attribute to form to enable users to have associated phone numbers

--- part2/bexercises/src/App.jsx
+++ part2/bexercises/src/App.jsx
@@ -1,58 +1,77 @@
 import { useState } from 'react'
 
-const DisplayPerson = ({ name }) => {
+const DisplayPerson = ({ name, number }) => {
   return (
-    <p style={{margin: 0}}>{name}</p>
+    <div>
+      {name} {number}
+    </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567',
+      id: 0,}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const [idTracker, setIdTracker] = useState(0)
 
   const addName = (event) => {
     event.preventDefault()
-    let nameSeen = false
+    const duplicateExists = checkDuplicateName()
+    if (!duplicateExists) {
+      //create a new person
+      const newPerson = [{ name: newName, number: newNumber, id: idTracker}]
+      //create a new array with new person added
+      setPersons(persons.concat(newPerson))
+      //resets the states of setNewName and NewNumber
+      setNewName('')
+      setNewNumber('')
+      //increments id by 1
+      setIdTracker(idTracker + 1)
+    }
+  }
 
+  const checkDuplicateName = () => {
+    let nameSeen = false
     persons.map((person) => {
       if (person.name === newName) {
         nameSeen = true
         alert(`${newName} is already added to phonebook`)
       }
     }) 
-    
-    if (!nameSeen) {
-      const newPerson = [{ name: newName, id: idTracker}]
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setIdTracker(idTracker + 1)
-    }
-
-    console.log(persons)
+    return nameSeen
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          <div>name: <input value={newName} onChange={handleNameChange}/></div>
+          <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <DisplayPerson key={person.id} name={person.name}/>)}
+      {persons.map(person => <DisplayPerson key={person.id} name={person.name} number={person.number}/>)}
     </div>
   )
+
 }
 
 export default App
