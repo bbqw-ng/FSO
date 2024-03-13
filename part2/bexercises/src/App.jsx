d219ef1c83d5d56ba322059e50594261240081c5
bbqw_ng <brwng902@gmail.com>
2024-03-12 23:52:11 -0400

    completed the name filter 2.9

--- part2/bexercises/src/App.jsx
+++ part2/bexercises/src/App.jsx
@@ -1,26 +1,21 @@
 import { useState } from 'react'
-
-const DisplayPerson = ({ name, number }) => {
-  return (
-    <div>
-      {name} {number}
-    </div>
-  )
-}
+import DisplayPerson from './component/DisplayPerson.jsx'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567', id: 0},
-    { name: 'sugma dook', number: '69919919919-91991919196', id: 1},
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [idTracker, setIdTracker] = useState(0)
+  const [idTracker, setIdTracker] = useState(1)
+  const [nameFilter, setNameFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    const duplicateExists = checkDuplicateName()
-    if (!duplicateExists) {
+    if (!checkDuplicateName()) {
       //create a new person
       const newPerson = [{ name: newName, number: newNumber, id: idTracker}]
       //create a new array with new person added
@@ -30,6 +25,7 @@ const App = () => {
       setNewNumber('')
       //increments id by 1
       setIdTracker(idTracker + 1)
+      console.log(persons)
     }
   }
 
@@ -51,11 +47,39 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  
+  const handleNameFilterChange = (event) => {
+    setNameFilter(event.target.value)
+  }
+  
+  const showFiltered = () => {
+    if (nameFilter === '') {
+      //return all persons.
+      return (
+        <>
+          {persons.map(person => <DisplayPerson key={person.id} name={person.name} number={person.number}/>)}
+        </>
+      )
+    } else {
+      //here we need to return the first thing back into the App's return.
+      return (
+        <>
+          {persons.map(person => { if (person.name.toLowerCase().includes(nameFilter.toLowerCase())) { 
+            //now we need to return the actual content.
+            return (
+              <DisplayPerson key={person.id} name={person.name} number={person.number}/>
+            )
+          }})}
+        </>
+      )
+    }
+  }
+      
 
   return (
     <div>
       <h2>Phonebook</h2>
-      
+      <div>filter shown with <input value={nameFilter} onChange={handleNameFilterChange}/></div>
       <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
@@ -66,8 +90,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map(person => <DisplayPerson key={person.id} name={person.name} number={person.number}/>)}
+      <h3>Numbers</h3>
+      {showFiltered()}
     </div>
   )
 
---
+++ part2/bexercises/src/component/DisplayPerson.jsx
@@ -0,0 +1,9 @@
+const DisplayPerson = ({ name, number }) => {
+  return (
+    <div>
+      {name} {number}
+    </div>
+  )
+}
+
+export default DisplayPerson
